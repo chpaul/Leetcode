391426b3ea16112bb60c2af65140fad7ee5939f9
Chuan-Yih Yu <ml10411@hotmail.com>
2017-08-24 00:07:06 -0500

    Add two temp

---
+++ 02_AddTwoNumber.py
@@ -0,0 +1,25 @@
+class ListNode:
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+
+
+def addTwoNumbers(l1, l2):
+
+
+
+if __name__ == "__main__":
+    l1 = ListNode(0)
+    l1.next = ListNode(1)
+    l1.next = ListNode(3)
+
+    l2 = ListNode(4)
+    l2.next = ListNode(5)
+    l2.next = ListNode(3)
+
+    tar  = 6
+    a , b = addTwoNumbers(nums, tar)
+    print(a)
+    print(b)
+
+
