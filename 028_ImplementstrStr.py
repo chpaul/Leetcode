3deb49ca30bea92c6ef531683fcc15f4aa678851
CHPaul <ml10411@hotmail.com>
2017-09-06 17:20:46 -0500

    incomplete

---
+++ 028_ImplementstrStr.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+
+        if needle not in haystack: return -1
+        if needle == haystack: return 0;
+        idx = -1
+        haystackLen = len(haystack)
+        needleLen = len(needle)
+        if needleLen > haystackLen: return -1
+        if needle == None or needleLen == 0 : return 0
+
+        for i in range(0, haystackLen - needleLen+1):
+            if haystack[i] == needle[0]:
+                for j in range(1, needleLen):
+                    if haystack[i+j] != needle[j]:
+                        found = False
+
+
+        return idx
+
+
+if __name__ == "__main__":
+    solution = Solution
+    a = solution.strStr(solution, 'mississippi', 'issip')
+    print(a)
