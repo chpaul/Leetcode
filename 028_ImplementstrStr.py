e09b29ba82fb40d91f0ffa32c0430c15f9ec3803
Chuan-Yih Yu <ml10411@hotmail.com>
2017-09-06 21:46:26 -0500

    Completed

--- 028_ImplementstrStr.py
+++ 028_ImplementstrStr.py
@@ -5,23 +5,22 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-
         if needle not in haystack: return -1
         if needle == haystack: return 0;
-        idx = -1
         haystackLen = len(haystack)
         needleLen = len(needle)
         if needleLen > haystackLen: return -1
         if needle == None or needleLen == 0 : return 0
 
         for i in range(0, haystackLen - needleLen+1):
+            found = None
             if haystack[i] == needle[0]:
                 for j in range(1, needleLen):
                     if haystack[i+j] != needle[j]:
                         found = False
-
-
-        return idx
+                if found != False:
+                    return i
+        return -1
 
 
 if __name__ == "__main__":
